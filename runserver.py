df4d4a617740da6f2b3736c764bf6bdbb93ff0f8
Joshua Bharathi <blitz04.dev@gmail.com>
2022-04-01 03:20:54 +0530

    setup audio downloading from yt

--- runserver.py
+++ runserver.py
@@ -2,9 +2,9 @@ from server import app
 
 import json
 runconfig = json.load(open('runconfig.json'))
-DEBUG = runconfig.get('debug', False)
-HOST = runconfig.get('host', '0.0.0.0')
-PORT = runconfig.get('port', 5000)
+DEBUG = runconfig.get('DEBUG', False)
+HOST = runconfig.get('HOST', '0.0.0.0')
+PORT = runconfig.get('PORT', 5000)
 
 if __name__ == '__main__':
     app.run(debug=DEBUG, host=HOST, port=PORT)
--- server/views.py
+++ server/views.py
@@ -1,8 +1,9 @@
-from flask import jsonify, request, render_template
+from flask import jsonify, request, render_template, send_from_directory
 
 from .app import app
 from ytmusic.recommendations import get_recommendations
 from ytmusic.search import get_search_results
+from ytmusic.download import DOWNLOAD_PATH, download_audio
 
 
 @app.route('/')
@@ -26,3 +27,11 @@ def __search():
     lim = request.args.get('lim', 50, int)
     if not query: return jsonify({'error': 'specify query da bunda'}), 400
     return jsonify(get_search_results(query, lim))
+
+@app.route('/download')
+def __download():
+    video_id = request.args.get('video_id')
+    if not video_id: return jsonify({'error': 'specify video_id da bunda'}), 400
+    fname = download_audio(video_id, DOWNLOAD_PATH)
+    print(DOWNLOAD_PATH.split('/')[-1], fname)
+    return send_from_directory('.' + DOWNLOAD_PATH, fname) # send_from_dir takes app instnatiated file as root. we need to go one dir out
