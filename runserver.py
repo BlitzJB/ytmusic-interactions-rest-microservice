4367e154f6536246767623895b9d1ece576972c1
Joshua Bharathi <blitz04.dev@gmail.com>
2022-04-01 01:40:00 +0530

    update to use runconfig

--- runserver.py
+++ runserver.py
@@ -1,4 +1,10 @@
 from server import app
 
+import json
+runconfig = json.load(open('runconfig.json'))
+DEBUG = runconfig.get('debug', False)
+HOST = runconfig.get('host', '0.0.0.0')
+PORT = runconfig.get('port', 5000)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=DEBUG, host=HOST, port=PORT)
