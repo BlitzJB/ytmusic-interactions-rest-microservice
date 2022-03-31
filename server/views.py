69269a00ba6b0c4c2be5782f20d4ddbf9da348f3
Joshua Bharathi <blitz04.dev@gmail.com>
2022-04-01 02:00:12 +0530

    handling for when user does 2000IQ moves (messes up)

--- server/views.py
+++ server/views.py
@@ -4,6 +4,7 @@ from .app import app
 from ytmusic.recommendations import get_recommendations
 from ytmusic.search import get_search_results
 
+
 @app.route('/')
 def __index():
     return render_template('docs.html')
@@ -16,10 +17,12 @@ def __check():
 def __recommendations():
     video_id = request.args.get('video_id')
     lim = request.args.get('lim', 50, int)
+    if not video_id: return jsonify({'error': 'specify video_id da bunda'}), 400
     return jsonify(get_recommendations(video_id, lim))
 
 @app.route('/search')
 def __search():
     query = request.args.get('query')
     lim = request.args.get('lim', 50, int)
+    if not query: return jsonify({'error': 'specify query da bunda'}), 400
     return jsonify(get_search_results(query, lim))
