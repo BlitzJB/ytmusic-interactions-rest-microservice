1f6c758709cdb26a9bbd2f511e7a05ffb88bc4d7
Joshua Bharathi <blitz04.dev@gmail.com>
2022-04-01 03:23:35 +0530

    onmount

--- runserver.py
+++ runserver.py
@@ -1,10 +1,16 @@
 from server import app
 
+import os
+
 import json
 runconfig = json.load(open('runconfig.json'))
 DEBUG = runconfig.get('DEBUG', False)
 HOST = runconfig.get('HOST', '0.0.0.0')
 PORT = runconfig.get('PORT', 5000)
 
+def onmount():
+    os.mkdir(runconfig['DOWNLOADS_PATH'])
+
 if __name__ == '__main__':
+    onmount()
     app.run(debug=DEBUG, host=HOST, port=PORT)
